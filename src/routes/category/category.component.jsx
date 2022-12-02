1b9a70d66505e339b9bbdde051e11e2c8f574cd9
jegoree <jegor.mon@outlook.com>
2022-12-03 00:24:32 +0200

    Hotfix: Resolved the routing

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 
 import ProductCard from '../../components/product-card/product-card.component';
@@ -17,12 +17,15 @@ const Category = () => {
   }, [category, categoriesMap]);
 
   return (
-    <div className='category-contianer'>
-      {products &&
-        products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-    </div>
+    <Fragment>
+      <h2 className='title'>{category.toUpperCase()}</h2>
+      <div className='category-contianer'>
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </Fragment>
   );
 };
 
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -2,39 +2,11 @@ import { Outlet } from 'react-router-dom';
 
 import Categories from '../../components/categories/categories.component';
 
-const categories = [
-  {
-    id: 1,
-    title: 'New Arrivals',
-    imageUrl: 'https://i.ibb.co/rGYSsVP/new-arrivals.jpg',
-  },
-  {
-    id: 2,
-    title: 'By Age',
-    imageUrl: 'https://i.ibb.co/LP7BmRj/by-age.jpg',
-  },
-  {
-    id: 3,
-    title: 'By Manufacturer',
-    imageUrl: 'https://i.ibb.co/r6WPGkm/by-manufacturer.jpg',
-  },
-  {
-    id: 4,
-    title: 'All Products',
-    imageUrl: 'https://i.ibb.co/6v8XY5Z/all-products.jpg',
-  },
-  {
-    id: 5,
-    title: 'Best Sellers',
-    imageUrl: 'https://i.ibb.co/2SbSVBY/best-sellers.jpg',
-  },
-];
-
 const Home = () => {
   return (
     <div>
       <Outlet />
-      <Categories categories={categories} />;
+      <Categories />;
     </div>
   );
 };
